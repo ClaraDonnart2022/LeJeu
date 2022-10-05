64bf53fad26c06772852f2733872c82bced334c7
ClaraDonnart2022 <62743647+ClaraDonnart2022@users.noreply.github.com>
2022-10-05 10:21:39 +0200

    Add Class Game

--- LeJeu.py
+++ LeJeu.py
@@ -46,7 +46,76 @@ class Hand():
     def __str__(self):
         return " \n".join([str(i+1)+". "+str(c) for i,c in enumerate(self.cards)])
 
-#class Game():
+class Game():
+    """ Un jeu de LeJeu."""
+
+    def __init__(self):
+        players = ["Hadri", "Clarou"]
+
+        """ Définition des fonctions des cartes """
+
+        def play1():
+            print("Appliquer le play de la carte 1")
+
+        def play2():
+            print("Appliquer le play de la carte 2")
+
+        def play3():
+            print("Appliquer le play de la carte 3")
+
+        def play4():
+            print("Appliquer le play de la carte 4")
+            
+
+        """ Création des cartes """
+
+        a = Card(1,play1,"Je suis la carte 1",0)
+        b = Card(1, play2, "Je suis la carte 2",1)
+        c = Card(1,play3,"Je suis la carte 3",1)
+        d = Card(1, play4, "Je suis la carte 4",0)
+        l1 = [a,b,c,d]
+
+        a = Card(2,play1,"Je suis la carte 5",1)
+        b = Card(2, play2, "Je suis la carte 6",0)
+        c = Card(2,play3,"Je suis la carte 7",1)
+        d = Card(2, play4, "Je suis la carte 8",0)
+        l2 = [a,b,c,d]
+
+        """Création des deux decks de départ"""
+
+        deck1 = Deck(l1)
+        deck2 = Deck(l2)
+        deck1.shuffle()
+        deck2.shuffle()
+
+        self.current = Player(players[0],deck1)
+        self.other = Player(players[1], deck2)
+        self.discard = []
+
+    def turn(self):
+
+        self.current.cards.append(self.current.deck.draw())
+        print(f"C'est le tour de {self.current.name}. Voici tes cartes:")
+        rep = "j"
+        while rep == "j" and len(self.current.cards)!=0:
+            print(self.current.hand)
+            rep = input("Veux-tu jouer une carte (j) ou passer (p)?")
+            if rep == "j":
+                try :
+                    cardplay = int(input("Laquelle?"))   
+                    self.current.cards[cardplay-1].play()
+                    cardplayed = self.current.cards.pop(cardplay-1)
+                    if cardplayed.arg:
+                        self.current.arg +=1
+                    self.discard.append(cardplayed)
+                except ValueError as e:
+                    print("Grand fou met un numéro")
+                except IndexError as e:
+                    print("Un numéro que tu as plutôt coco")
+
+            print(f"Vous avez {self.current.arg} argument(s)")
+            self.current, self.other = self.other, self.current
+
 
 
 class Player():
@@ -63,6 +132,8 @@ class Player():
 
 if __name__ == "__main__":
     pass
+        
+
         
  
 
