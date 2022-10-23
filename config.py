6b7f7dd859678937a4be47f4f3cee2f494b6ee4a
HadrienNauroy <hadrien.nauroy@ensta-paris.fr>
2022-10-23 21:49:54 +0200

    add host file

---
+++ config.py
@@ -0,0 +1,8 @@
+"""
+#######################
+DO NOT SHARE ON GITHUB
+#######################
+"""
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
