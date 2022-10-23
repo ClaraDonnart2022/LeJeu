caecbd92c8cb86ec5a8daf6a7ef9b2fa6c2007f1
HadrienNauroy <hadrien.nauroy@ensta-paris.fr>
2022-10-23 21:51:21 +0200

    test

--- config.py
+++
@@ -1,8 +0,0 @@
-"""
-#######################
-DO NOT SHARE ON GITHUB
-#######################
-"""
-
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
